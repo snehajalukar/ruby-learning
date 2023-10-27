07c2a06cc83fc6695ae0b71e5456144fa284f1c0
Sneha Jalukar <shriyash1111@gmail.com>
2023-10-27 16:50:54 -0400

    clean up tictactoe

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,57 +1,21 @@
 class TicTacToe
-    # def initialize
+    def initialize
         @done = false
-
-        # @board = [
-        #     [nil, nil, nil],
-        #     [nil, nil, nil],
-        #     [nil, nil, nil]
-        # ]
-
         @board = [
             [" ", " ", " "],
             [" ", " ", " "],
             [" ", " ", " "]
         ]
+    end
 
-        # @board = [
-        #     ["x", " ", " "],
-        #     [" ", "x", " "],
-        #     [" ", " ", "x"]
-        # ]
-
-    # end
-
-
-    def self.printBoard
+    def printBoard
         puts "       | column 1 | column 2 | column 3 "
         puts " row 1 |       #{@board[0][0]}  |       #{@board[0][1]}  |       #{@board[0][2]} "
         puts " row 2 |       #{@board[1][0]}  |       #{@board[1][1]}  |       #{@board[1][2]} "
         puts " row 3 |       #{@board[2][0]}  |       #{@board[2][1]}  |       #{@board[2][2]} "
- 
-
-
-        # puts "row 1: #{@board[0]}\n"
-        # puts "row 2: #{@board[1]}\n"
-        # puts "row 3: #{@board[2]}\n"
-
     end
 
-
-    # @board = [
-    #     [00, 01, 02],
-    #     [10, 11, 12],
-    #     [20, 21, 22]
-    # ]
-
-    
-    def self.checkWinner
-        # @board.each do |n|
-        #     if n.include?(" ")
-        #         return
-        #     end
-        # end
-
+    def checkWinner
         # check columns
         if @board[0][0] != " " && @board[0][0] == @board[1][0] && @board[1][0] == @board[2][0]
             puts "\n#{@board[0][0]} wins!"
@@ -87,17 +51,8 @@ class TicTacToe
         end
     end
 
-
-
-    def self.gameDone
-        TicTacToe.checkWinner
-        # unless @done == true
-        #     @board.any? do |n|
-        #         if !n.include?(" ")
-        #             @done = true
-        #         end
-        #     end
-        # end
+    def gameDone
+        checkWinner
         unless @done == true
             if !@board.flatten.include?(" ")
                 puts "\nit's a tie"
@@ -107,15 +62,11 @@ class TicTacToe
         @done
     end
 
-
-
-    def self.eachTurn
-
-        # TicTacToe.gameDone
-
+    def eachTurn
+        gameDone
         while @done == false
             puts "\n\nBoard: "
-            TicTacToe.printBoard
+            printBoard
             puts "\nEnter x or o\n"
             response = gets.chomp
 
@@ -127,16 +78,12 @@ class TicTacToe
 
             @board[row][column] = response
             
-            TicTacToe.gameDone
+            gameDone
         end
-
-        TicTacToe.printBoard
-
+        printBoard
     end
-
-
-    TicTacToe.eachTurn
-
-
-
 end
+
+game = TicTacToe.new
+
+game.eachTurn
